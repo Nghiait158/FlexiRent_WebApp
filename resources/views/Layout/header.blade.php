435b0cde4f609fdfa6c8e4a915a8776511572e28
Vũ Thành Nhân <147362828+VuThanhNhan2003@users.noreply.github.com>
2024-09-29 17:04:01 +0700

    it has header and footer

--- resources/views/Layout/header.blade.php
+++ resources/views/Layout/header.blade.php
@@ -1,1 +1,16 @@
-<header></header>
+    <!-- Logo và Navigation Bar -->
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href="#" target="_blank" rel="noopener noreferrer"><img src="image/Logo.png" alt="Logo"></a>
+            </div>
+            <nav class="nav-list">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Landlords</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contacts</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
