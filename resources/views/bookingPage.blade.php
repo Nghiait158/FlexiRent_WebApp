97260212ca64aee9ccb9748a31ad58c71c3f82c9
Nghia <trinhvantrungnghia@gmail.com>
2024-09-29 19:54:02 +0700

    fix header and footer, move css to public

--- resources/views/bookingPage.blade.php
+++ resources/views/bookingPage.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link rel="stylesheet"  href="{{('/Frontend/css/style.css')}}" /> --}}
-    <link rel="stylesheet" href="{{ asset('/Frontend/css/bookingPage.css') }}">
-    
-    {{-- ---------Font Molle------------ --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Molle&display=swap" rel="stylesheet">
-
-    <title>HomeUp App</title>
-</head>
-<body>
-
+@extends('Layout/header_footer')
+@section('content')
     <main>
         <div class="searchBar_booking">
             <div class="searchPart1">
@@ -86,9 +70,3 @@
             
         </div>
     </main>
-
-
-        
-
-</body>
-</html>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/bookingPage', function () {
+    return view('bookingPage');
+});
+
+
 Route::get('/', function () {
-    return view('Homepage');
+    return view('Layout.header_footer');
 });
 
 
