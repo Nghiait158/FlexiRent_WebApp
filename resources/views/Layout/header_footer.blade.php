8998643ff2348ef5f1079966e1e111cf7bed35b6
Minh Nhat <nguyenvuminhnhat191003@gmail.com>
2024-10-02 01:09:45 +0700

    Add slide house
    add slide house + fix font family Landlord

--- resources/views/Layout/header_footer.blade.php
+++ resources/views/Layout/header_footer.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HomeUp </title>
+    <title>FlexiRent</title>
     <link rel="stylesheet" href="{{('/Frontend/css/footer.css')}}" />
     <link rel="stylesheet" href="{{('/Frontend/css/header.css')}}" />
 
@@ -23,7 +23,7 @@
         
         <div class="header-container">
             <div class="header-logo">
-                <a href="/resources/views/Layout/Homepage.html">FlexiRent</a>
+                <a href="{{URL::to('/HomePage')}}">FlexiRent</a>
             </div>
 
             <div class="header-nav">
@@ -44,7 +44,7 @@
         @yield('content')
        
     </main>
-    {{-- <div style="height: 550px; background-color=red;" > --}}
+    {{-- <div style="height: 550px; background-color:red;" > --}}
         
        
         
