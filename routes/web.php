e13e1f5c302c66597a86e268ec67a1df2a6fe941
Nghia <trinhvantrungnghia@gmail.com>
2024-10-06 21:42:27 +0700

    create breeze for auth

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookingPageController;
-use App\Http\Controllers\PropertyDetailsPageController;
 
-use App\Http\Controllers\MapController;
-// --------FrontEnd-------------------------
 Route::get('/', function () {
-    return view('Homepage');
-});
-Route::get('/HomePage', function () {
-    return view('Homepage');
+    return view('welcome');
 });
 
-Route::get('/Landlord', function () {
-    return view('Landlords');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/Blog', function () {
-    return view('Blog');
-});
-
-Route::get('/PropertyDetails', function () {
-    return view('PropertyDetails');
-});
-Route::get('/CheckoutPageA', function () {
-    return view('CheckoutPageA');
-});
-
-// Route::get('/map', [MapController::class, 'generateMapIframe']);
-
-Route::get('/bookingPage', [BookingPageController::class, 'showMap']);
-Route::get('/PropertyDetails', [PropertyDetailsPageController::class, 'showMap']);
-
-
-
+require __DIR__.'/auth.php';
