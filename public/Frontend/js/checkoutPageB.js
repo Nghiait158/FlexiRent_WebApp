6521a9de4ea8b59e0e34a3878fdcf1622cd3768b
Minh Nhat <nguyenvuminhnhat191003@gmail.com>
2024-10-10 22:59:34 +0700

    update calendar

--- public/Frontend/js/checkoutPageB.js
+++ public/Frontend/js/checkoutPageB.js
@@ -71,48 +71,5 @@ radioButtons.forEach(radio => {
     });
 });
 
-$(function() {
-    var dateFormat = "mm/dd/yy",
-        minDays = 14, // Minimum days between "from" and "to" dates
-        from = $("#from")
-            .datepicker({
-                defaultDate: "+1w",
-                changeMonth: true,
-                changeYear: true,
-                numberOfMonths: 1
-            })
-            .on("change", function() {
-                var selectedFromDate = getDate(this);
-                if (selectedFromDate) {
-                    // Add 14 days to the selected "from" date for the "to" datepicker's minDate
-                    selectedFromDate.setDate(selectedFromDate.getDate() + minDays);
-                    to.datepicker("option", "minDate", selectedFromDate);
-                }
-            }),
-        to = $("#to")
-            .datepicker({
-                defaultDate: "+1w",
-                changeMonth: true,
-                changeYear: true,
-                numberOfMonths: 1
-            })
-            .on("change", function() {
-                var selectedToDate = getDate(this);
-                if (selectedToDate) {
-                    // Subtract 14 days from the selected "to" date for the "from" datepicker's maxDate
-                    selectedToDate.setDate(selectedToDate.getDate() - minDays);
-                    from.datepicker("option", "maxDate", selectedToDate);
-                }
-            });
 
-    function getDate(element) {
-        var date;
-        try {
-            date = $.datepicker.parseDate(dateFormat, element.value);
-        } catch (error) {
-            date = null;
-        }
-        return date;
-    }
-});
 
--- public/Frontend/js/homeBlogSlider.js
+++ public/Frontend/js/homeBlogSlider.js
@@ -1,25 +1,43 @@
 var swiper = new Swiper(".blogSwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    rewind: true,
-    coverflowEffect: {
-      rotate: 10,
-      stretch: 0,
-      depth: 200,
-      modifier: 1,
-      slideShadows: true,
-    },
-    scrollbar: {
-      el: ".swiper-scrollbar",
-      hide: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  rewind: true,
+  coverflowEffect: {
+    rotate: 10,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
   },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+// JS for calendar
+const inputs = document.querySelectorAll('.pick-date input');
+
+// Add event listeners for focus and blur
+inputs.forEach(input => {
+  input.addEventListener('focus', () => {
+    input.placeholder = ''; // Clear placeholder on focus
   });
 
+  input.addEventListener('blur', () => {
+    if (input.value === '') {
+      input.placeholder = input.getAttribute('data-placeholder'); // Restore placeholder if empty
+    }
+  });
 
-  
+  // Store the original placeholder in a data attribute
+  input.setAttribute('data-placeholder', input.placeholder);
+});
+
