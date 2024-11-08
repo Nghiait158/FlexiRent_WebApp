512f5d4cfebbddb5bfcc27f75f79b49f0a1b70ee
NguyenVuMinhNhat <154681403+NguyenVuMinhNhat@users.noreply.github.com>
2024-11-08 18:23:16 +0700

    fix search bar

--- public/Frontend/js/calendar.js
+++ public/Frontend/js/calendar.js
@@ -24,13 +24,13 @@ $(function() {
                 changeMonth: true,
                 changeYear: true,
                 numberOfMonths: 1,
-                minDate: minDays, // Prevent booking before 14 days from today
+                minDate: minDays, // Prevent booking before 7 days from today
                 dateFormat: dateFormat // Set the date format
             })
             .on("change", function() {
                 var selectedToDate = getDate(this);
                 if (selectedToDate) {
-                    // Subtract 14 days from the selected "to" date for the "from" datepicker's maxDate
+                    // Subtract 7 days from the selected "to" date for the "from" datepicker's maxDate
                     selectedToDate.setDate(selectedToDate.getDate() - minDays);
                     from.datepicker("option", "maxDate", selectedToDate);
                 }
